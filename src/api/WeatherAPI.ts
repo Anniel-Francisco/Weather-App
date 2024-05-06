b013d3eb5b577b1c7345c9a589761ccbaff290d9
Anniel <annielfranciscoreyes@gmail.com>
2024-05-06 02:44:06 -0400

    chore(weather-app): change in WeatherAPI

--- src/api/WeatherAPI.ts
+++ src/api/WeatherAPI.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   async getWeather(location: string) {
-    return axios.get(`http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${location}&aqi=no`).then(data => data);
+    return axios.get(`https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${location}&aqi=no`).then(data => data);
   },
 };
